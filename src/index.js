de39570c44802a7417088b2240303bdaf2910ef1
nangbx <nangbx01@gmail.com>
2022-05-20 16:47:53 +0700

    Add UI

--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// scroll bar
+import "simplebar/src/simplebar.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+
+//
+import App from "./App";
+
+import reportWebVitals from "./reportWebVitals";
+
+// ----------------------------------------------------------------------
+
+ReactDOM.render(
+	<HelmetProvider>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</HelmetProvider>,
+	document.getElementById("root")
 );
+
+// If you want to enable client cache, register instead.
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
